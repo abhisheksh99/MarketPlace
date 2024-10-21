1649247f513cd267901ac246950688b5d379a48e
abhisheksh99 <abhishekleo1234@gmail.com>
2024-10-21 11:40:48 -0400

    Firebase added and firestore data and fettching data added

--- MarketPlace/apps/Screens/AddPostScreen.tsx
+++ MarketPlace/apps/Screens/AddPostScreen.tsx
@@ -1,9 +1,41 @@
-import React from 'react'
+import React, { useEffect, useState } from "react";
+import { collection, getDocs, getFirestore } from "firebase/firestore";
+import { app } from "../../firebaseConfig";
 
 const AddPostScreen = () => {
-  return (
-    <div>AddPostScreen</div>
-  )
-}
+  const db = getFirestore(app);
+  const [categoryList, setCategoryList] = useState([]); 
 
-export default AddPostScreen
+  const getCategoryList = async () => {
+    try {
+      const querySnapshot = await getDocs(collection(db, "Category"));
+      const categories = []; // Temporary array to hold categories
+
+      querySnapshot.forEach((doc) => {
+        
+        categories.push({ id: doc.id, ...doc.data() }); // Collecting category data
+      });
+
+      setCategoryList(categories); // Updating state with the collected data
+    } catch (error) {
+      console.error("Error fetching categories: ", error);
+    }
+  };
+
+  useEffect(() => {
+    getCategoryList();
+  }, []);
+
+  return (
+    <div>
+      <h1>Add Post Screen</h1>
+      <ul>
+        {categoryList.map((category) => (
+          <li key={category.id}>{category.name}</li>
+        ))}
+      </ul>
+    </div>
+  );
+};
+
+export default AddPostScreen;
---
+++ MarketPlace/firebaseConfig.tsx
@@ -0,0 +1,15 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyDc1gOo1b5JEngoYlD_2VQWVC40-1-X9G8",
+  authDomain: "marketplace-24735.firebaseapp.com",
+  projectId: "marketplace-24735",
+  storageBucket: "marketplace-24735.appspot.com",
+  messagingSenderId: "410072416248",
+  appId: "1:410072416248:web:b95e6dfc3fce307733a00d"
+};
+
+// Initialize Firebase
+export const app = initializeApp(firebaseConfig);
